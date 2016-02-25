cec013760611038d1771a5b9937ca57b35ae8ee1
Yash Chandarana <yashchandarana@Yashs-MacBook-Pro.local>
2016-02-25 22:16:44 +0100

    Initial commit

---
+++ app/src/main/java/app/nanodegree/android/com/androidnanodegreeproject/MainActivity.java
@@ -0,0 +1,81 @@
+package app.nanodegree.android.com.androidnanodegreeproject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void spotifyStreamerPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch spotify streamer app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+    public void scoresAppPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch scores app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+    public void libraryAppPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch library app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+    public void buildItBiggerPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch build it bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+    public void xyzReaderPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch XYZ reader app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+    public void myOwnAppPressed(View view){
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
+    }
+}
